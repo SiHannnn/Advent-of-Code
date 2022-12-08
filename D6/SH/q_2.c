a8bb4847c987c3633d1de650d75ab1a9eca7948a
Si Han <sihantan19980120@gmail.com>
2022-12-09 02:19:23 +0800

    Upload SH D6 doced verision

--- D6/SH/q_2.c
+++ D6/SH/q_2.c
@@ -11,13 +11,14 @@ int main(void){
     scanf("%s", input_string);
 
     int output = 0;
-
-    char *start = input_string;
+    
+    //Stop before the last set of chars
     char *end = input_string_pntr + strlen(input_string) - (DISTINCT_CHAR_CNT - 1);
     while (input_string_pntr < end)
     {
         output = check_match(input_string, input_string_pntr);
 
+        //Exit loop when there is a valid answer otherwise search for next set
         if(output){
             break;
         }else{
@@ -35,12 +36,15 @@ int check_match(char *input_string, char *input_string_pntr){
     for(int i = 1; i < DISTINCT_CHAR_CNT; ++i){
         char *check_pntr = input_string_pntr + i;
 
+        //Reset checking pointers
         char *check_start = input_string_pntr;
         char *check_end = input_string_pntr + (DISTINCT_CHAR_CNT - 1);
 
         while (check_start < check_end)
         {
+            //Skip checking itself
             if(check_start - check_pntr != 0){
+                //Return 0 when there is a match
                 if(*check_pntr == *check_start){
                     return 0;
                 }
@@ -49,5 +53,6 @@ int check_match(char *input_string, char *input_string_pntr){
         }                    
     }
 
+    //Return the answer
     return input_string_pntr + (DISTINCT_CHAR_CNT - 1) - input_string + 1;
 }
